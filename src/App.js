b128608748f09db7d9b7ec47180e3aed5a0fe85f
mansi <kachhiyamanshi15042810@gmail.com>
2022-09-08 16:40:46 +0530

    Initial commit for react- redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Todos from "./componant/Todos"
+import Atte from "./componant/Atte";
+import Calender from"./componant/Calender"
+import moment from "moment";
+// import moment from "react-moment";
 
 function App() {
+    // let startDate = moment().add(-365, 'days');
+    // let dateRange = [startDate, moment()];
+    //
+    // let data = Array.from(new Array(365)).map((_, index) => {
+    //     return {
+    //         date: moment(startDate).add(index, 'day'),
+    //         value: Math.floor(Math.random() * 100)
+    //     };
+    // });
+    let dataRange = [moment().add(-365,'days'),moment()]
+    // console.log(dataRange.toString())
+    // console.log(dataRange[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*<Atte range={dataRange} />*/}
+      <Calender range={dataRange}/>
+    </>
   );
 }
 
